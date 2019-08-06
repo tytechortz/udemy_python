37c548e494396e99aebfdc2266b1280143838e42
James Swank <jmswank7@gmail.com>
2019-08-06 08:28:21 -0600

    basics

--- beyond_basics/password_check.py
+++ beyond_basics/password_check.py
@@ -1,7 +1,8 @@
 correct_password = "python123"
+name = input("Enter name: ")
 password = input("Enter password: ")
 
 while correct_password != password:
     password = input("Wrong password. Enter password again: ")
 
-print("Logged in")
+print("Hi, {}, You are logged in".format(name))
