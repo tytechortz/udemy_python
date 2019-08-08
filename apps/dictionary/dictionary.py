fbf29808360ccc4bec662d2e96263af4d5fc55e0
James Swank <jmswank7@gmail.com>
2019-08-07 19:55:57 -0600

    finishes app

--- apps/dictionary/dictionary.py
+++ apps/dictionary/dictionary.py
@@ -7,11 +7,27 @@ def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif w.title() in data:
+        return data[w.title()]
+    elif w.upper() in data:
+        return data[w.upper()]
     elif len(get_close_matches(w, data.keys())) > 0:
-        return "Did you mean {} instead?".format(get_close_matches(w, data.keys())[0])
+        yn = input("Did you mean {} instead? Enter Y if yes, or N if no: ".format(get_close_matches(w, data.keys())[0]))
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn == "N":
+            return "The word doesn't exist."
+        else:
+            return "Need Y or N."
     else:
         return "The word doesn't exist."
 
 word = input("Enter word: ")
 
-print(translate(word))
+output = translate(word)
+
+if type(output) == list:  
+    for item in output:
+        print(item)
+else:
+    print(output)
