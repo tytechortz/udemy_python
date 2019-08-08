6c812cdca5c75ec74aedbf01bc1e3fbe37c531df
James Swank <jmswank7@gmail.com>
2019-08-07 19:31:49 -0600

    gets close match

--- apps/dictionary/dictionary.py
+++ apps/dictionary/dictionary.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -6,6 +7,8 @@ def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        return "Did you mean {} instead?".format(get_close_matches(w, data.keys())[0])
     else:
         return "The word doesn't exist."
 
