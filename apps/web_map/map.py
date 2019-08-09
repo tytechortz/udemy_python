1049a47572cf60e326905d9054e3d5acf9192635
James Swank <jmswank7@gmail.com>
2019-08-09 08:34:14 -0600

    adds marker

--- apps/web_map/map.py
+++ apps/web_map/map.py
@@ -1,4 +1,9 @@
 import folium
 map = folium.Map(location=[40, -104], zoom_start=6)
+
+fg = folium.FeatureGroup(name="My Map")
+fg.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi", icon=folium.Icon(color='green')))
+map.add_child(fg)
+
 map.save("Map1.html")
 
