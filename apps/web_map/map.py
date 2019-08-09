fba66e159128d420e73af1760ced1e36fb2ae02a
James Swank <jmswank7@gmail.com>
2019-08-09 09:15:33 -0600

    adds colors by elevation

--- apps/web_map/map.py
+++ apps/web_map/map.py
@@ -6,12 +6,20 @@ lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
 
-map = folium.Map(location=[40, -104], zoom_start=6)
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
+map = folium.Map(location=[40, -104], zoom_start=5)
 
 fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el)+" m", icon=folium.Icon(color='green')))
+    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el)+" m", icon=folium.Icon(color=color_producer(el))))
 
 map.add_child(fg)
 
