56d5786a0a682c5de956c0b469d717260546034a
James Swank <jmswank7@gmail.com>
2019-08-09 09:03:58 -0600

    adds elev to popup

--- apps/web_map/map.py
+++ apps/web_map/map.py
@@ -4,13 +4,14 @@ import pandas
 data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
 
 map = folium.Map(location=[40, -104], zoom_start=6)
 
 fg = folium.FeatureGroup(name="My Map")
 
-for lat, lon in zip(lat, lon):
-    fg.add_child(folium.Marker(location=[lat, lon], popup="Hi", icon=folium.Icon(color='green')))
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el)+" m", icon=folium.Icon(color='green')))
 
 map.add_child(fg)
 
