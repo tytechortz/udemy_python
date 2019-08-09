5ccd3e7a1c0b17a92fd51effb97d09d604aa9eec
James Swank <jmswank7@gmail.com>
2019-08-09 08:39:44 -0600

    for loop for markers

--- apps/web_map/map.py
+++ apps/web_map/map.py
@@ -2,7 +2,10 @@ import folium
 map = folium.Map(location=[40, -104], zoom_start=6)
 
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi", icon=folium.Icon(color='green')))
+
+for coordinates in [[38.2, -99.1],[40, -106]]:
+    fg.add_child(folium.Marker(location=coordinates, popup="Hi", icon=folium.Icon(color='green')))
+
 map.add_child(fg)
 
 map.save("Map1.html")
