394f68b9c36eac05e936d62910010c2d1af9987f
James Swank <jmswank7@gmail.com>
2019-08-12 07:25:56 -0600

    button works

--- apps/tkinter_gui/script.py
+++ apps/tkinter_gui/script.py
@@ -2,7 +2,10 @@ from tkinter import *
 
 window=Tk()
 
-b1=Button(window,text="Execute")
+def km_to_miles():
+    print("success")
+
+b1=Button(window,text="Execute",command=km_to_miles)
 b1.grid(row=0,column=0)
 
 e1=Entry(window)
