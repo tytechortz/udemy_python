a3375f2bf49205caf98b12433d4c4740a72e45e6
James Swank <jmswank7@gmail.com>
2019-08-12 07:42:30 -0600

    converts to grams

---
+++ apps/tkinter_gui/kgtolbs.py
@@ -0,0 +1,28 @@
+from tkinter import *
+
+window=Tk()
+
+def kg_to_grams():
+    print(e1_value.get())
+    grams=float(e1_value.get())*1000
+    t1.insert(END,grams)
+
+
+
+b1=Button(window,text="Convert",command=kg_to_grams)
+b1.grid(row=0,column=3)
+
+e1_value=StringVar()
+e1=Entry(window,textvariable=e1_value)
+e1.grid(row=0,column=2)
+
+t1=Text(window,height=1,width=20)
+t1.grid(row=1,column=1)
+
+t2=Text(window,height=1,width=20)
+t2.grid(row=1,column=2)
+
+t3=Text(window,height=1,width=20)
+t3.grid(row=1,column=3)
+
+window.mainloop()
