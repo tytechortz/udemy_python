02f4e2b1b0c1b7b74224c094366cdadfd27e950e
James Swank <jmswank7@gmail.com>
2019-08-12 07:50:18 -0600

    fixes bugs

--- apps/tkinter_gui/kgtolbs.py
+++ apps/tkinter_gui/kgtolbs.py
@@ -5,15 +5,21 @@ window=Tk()
 def kg_to_grams():
     print(e1_value.get())
     grams=float(e1_value.get())*1000
+    t1.delete("1.0", END)
     t1.insert(END,grams)
     pounds=float(e1_value.get())*2.20462
+    t2.delete("1.0", END)
     t2.insert(END,pounds)
     ounces=float(e1_value.get())*35.274
+    t3.delete("1.0", END)
     t3.insert(END,ounces)
 
 
 b1=Button(window,text="Convert",command=kg_to_grams)
 b1.grid(row=0,column=3)
+
+e1=Label(window,text="Kg")
+e1.grid(row=0,column=0)
 
 e1_value=StringVar()
 e1=Entry(window,textvariable=e1_value)
