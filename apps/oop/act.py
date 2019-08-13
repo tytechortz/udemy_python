da6cd44bc16025c6fd3aaf2f4fecdccea71a65bd
James Swank <jmswank7@gmail.com>
2019-08-12 20:54:15 -0600

    finishes oop

---
+++ apps/oop/act.py
@@ -0,0 +1,32 @@
+class Account:
+
+    def __init__(self, filepath):
+        self.filepath=filepath
+        with open(filepath, 'r') as file:
+            self.balance=int(file.read())
+
+    def withdraw(self, amount):
+        self.balance=self.balance - amount
+        
+    def deposit(self, amount):
+        self.balance=self.balance + amount
+
+    def commit(self):
+        with open(self.filepath, 'w') as file:
+            file.write(str(self.balance))
+
+class Checking(Account):
+    
+    type="checking"
+
+    def __init__(self, filepath, fee):
+        Account.__init__(self, filepath)
+        self.fee=fee
+
+    def transfer(self, amount):
+        self.balance=self.balance - amount - self.fee
+
+checking=Checking("balance.txt", 1)
+checking.transfer(55)
+print(checking.balance)
+checking.commit()
